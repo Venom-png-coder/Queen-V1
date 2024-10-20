a3d9203b7233e418ff1c771cc25eeff2756aba0f
Venom-png-coder <mwanzamaurice91@gmail.com>
2024-10-20 08:36:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '254748423791'
+global.nomerowner = ["254769286210"]
 
 // Apikey 
 
